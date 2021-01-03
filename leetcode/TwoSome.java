36c6f50f655a487a2018b95d4870a4327fd1b3ba
taekrigi <taekrigi@taekrigiui-MacBookPro.local>
2021-01-03 06:04:15 -0800

    added TwoSme Class

---
+++ leetcode/TwoSome.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/two-sum/
+
+public class TwoSome {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) return new int[] { i, j };
+            }
+        }
+        
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
