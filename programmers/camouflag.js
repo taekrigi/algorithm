8884c540390552443b5a2a93c43e157592f53bdf
taekrigi <taekrigi@taekrigiui-MacBookPro.local>
2021-01-05 07:04:31 -0800

    added camouflag url

--- programmers/camouflag.js
+++ programmers/camouflag.js
@@ -1,3 +1,5 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42578?language=javascript
+
 function solution(clothes) {
   const sizesPerCloth = clothes.reduce((acc, cur) => {
     const [name, type] = cur
