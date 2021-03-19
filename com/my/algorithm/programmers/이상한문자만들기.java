f0d6fd91b83b78e1814fe438db2fc2ccba130dfb
taekrigi <taekrigi@gmail.com>
2021-03-19 13:12:24 +0900

    이상한 문자 만들

---
+++ com/my/algorithm/programmers/이상한문자만들기.java
@@ -0,0 +1,35 @@
+package my.algorithm.programmers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 이상한문자만들기 {
+	
+	public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        String[] split = s.split("\\s+");
+        List<Integer> indicesOfSpace = getIndicesOfSpace(s);
+        
+        for (int i = 0; i < split.length; i++) {
+            for (int j = 0; j < split[i].length(); j++) {
+                String str = String.valueOf(split[i].charAt(j));
+                sb.append(j % 2 != 0 ? str.toLowerCase() : str.toUpperCase());
+            }
+        }
+        
+        for (int i = 0; i < indicesOfSpace.size(); i++) {
+            sb.insert(indicesOfSpace.get(i), " ");
+        }
+        
+        return sb.toString();
+    }
+    
+    private List<Integer> getIndicesOfSpace(String s) {
+        List<Integer> indices = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') indices.add(i);
+        }
+        return indices;
+    }
+
+}
