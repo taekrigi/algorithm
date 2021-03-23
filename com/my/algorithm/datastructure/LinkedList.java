89fd2177f356a14277fc9c05e2d5ea90257e9f6b
taekrigi <taekrigi@gmail.com>
2021-03-23 20:52:19 +0900

    LinkedList!

--- com/my/algorithm/datastructure/LinkedList.java
+++ com/my/algorithm/datastructure/LinkedList.java
@@ -2,10 +2,9 @@ package my.algorithm.datastructure;
 
 public class LinkedList<T> {
 	
-	class Node {
+	private class Node {
 		private T value;
 		private Node next;
-		private boolean isLast;
 		
 		public Node(T value) {
 			this.value = value;
@@ -15,10 +14,6 @@ public class LinkedList<T> {
 			return value;
 		}
 
-		public void setValue(T value) {
-			this.value = value;
-		}
-
 		public Node getNext() {
 			return next;
 		}
@@ -26,43 +21,96 @@ public class LinkedList<T> {
 		public void setNext(Node next) {
 			this.next = next;
 		}
-
-		public boolean isLast() {
-			return isLast;
-		}
-
-		public void setLast(boolean isLast) {
-			this.isLast = isLast;
-		}
 	}
 	
 	private Node first;
 	private Node current;
+	private int size;
 	
 	public void add(T t) {
+		Node next = new Node(t);
+		
 		if (first == null) {
-			first = new Node(t);
+			first = next;
 			current = first;
-		} else {
-			current.setNext(new Node(t));
-			current = current.getNext();
-		}
+			size = 1;
+			return;
+		} 
+		
+		current.setNext(next);
+		current = current.getNext();
+		size += 1;
 	}
 	
 	public T get(int index) throws Exception {
+		if (index >= size) throw new ArrayIndexOutOfBoundsException(index);
+		
 		Node node = first;
-		
-		if (index == 0) return node.getValue();
-		
+
 		for (int i = 1; i <= index; i++) {
 			node = node.getNext();
 		}
 		
-		if (node == first) {
-			throw new Exception("Index out of array : " + index);
+		return node.getValue();
+	}
+	
+	public void remove(int index) {
+		if (index >= size) throw new ArrayIndexOutOfBoundsException(index);
+		
+		Node node = first;
+		Node prev = null;
+
+		if (index == 0) {
+			first = node.getNext();
+			size--;
+			return;
 		}
 		
-		return node.getValue();
+		for (int i = 1; i <= index; i++) {
+			prev = node;
+			node = node.getNext();
+		}
+		
+		prev.setNext(node.getNext());
+		size--;
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		
+		Node node = first;
+		
+		sb.append('[');
+		for (int i = 0; i < size; i++) {
+			sb.append(node.getValue() + (isLastIndex(i, size) ? "" : ", "));
+			node = node.getNext();
+		}
+		
+		sb.append(']');
+		
+		return sb.toString();
+	}
+	
+	private boolean isLastIndex(int i, int size) {
+		return i == size - 1;
+	}
+	
+	public static void main(String[] args) {
+		LinkedList<Integer> list = new LinkedList<>();
+		list.add(5);
+		list.add(3);
+		list.add(2);
+		list.add(5);
+		list.add(10);
+		list.remove(0);
+		list.remove(list.size() - 1);
+		System.out.println(list.size());
+		System.out.println(list);
 	}
 }
 
