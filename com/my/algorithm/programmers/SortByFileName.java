38fe83db682873d2271921edfd4e88545a992774
taekrigi <taekrigi@gmail.com>
2021-04-01 20:47:58 +0900

    removed unused import

--- com/my/algorithm/programmers/SortByFileName.java
+++ com/my/algorithm/programmers/SortByFileName.java
@@ -4,8 +4,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.junit.jupiter.api.Test;
-
 public class SortByFileName {
 	
 	public String[] solution(String[] files) {
