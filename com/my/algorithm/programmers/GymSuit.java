4fd29852437f7430ab50dbd0781d532a64887991
taekrigi <taekrigi@gmail.com>
2021-04-03 20:15:19 +0900

    gymsuit

--- com/my/algorithm/programmers/GymSuit.java
+++ com/my/algorithm/programmers/GymSuit.java
@@ -7,34 +7,34 @@ import java.util.Set;
 public class GymSuit {
 	
 	 public int solution(int n, int[] lost, int[] reserve) {
-	        Set<Integer> lostSet = toSet(lost);
-	        Set<Integer> reserveSet = toSet(reserve);
-	        
-	        int additionalCount = 0;
-	        
-	        for (int i = 0; i < lost.length; i++) {
-	            int lostStudent = lost[i];
-	            
-	            if (reserveSet.contains(lostStudent)) {
-	                reserveSet.remove(lostStudent);
-	                additionalCount++;
-	            } else if (reserveSet.contains(lostStudent - 1) && !lostSet.contains(lostStudent - 1)) {
-	                reserveSet.remove(lostStudent - 1);
-	                additionalCount++;
-	            } else if (reserveSet.contains(lostStudent + 1) && !lostSet.contains(lostStudent + 1)) {
-	                reserveSet.remove(lostStudent + 1);
-	                additionalCount++;
-	            }
-	        }
-	        
-	        return n - lost.length + additionalCount;
-	    }
-	    
-	    private Set<Integer> toSet(int[] array) {
-	        Set<Integer> set = new HashSet<>();
-	        for (int i = 0; i < array.length; i++) {
-	            set.add(array[i]);
-	        }
-	        return set;
-	    }
+        Set<Integer> lostSet = toSet(lost);
+        Set<Integer> reserveSet = toSet(reserve);
+        
+        int additionalCount = 0;
+        
+        for (int i = 0; i < lost.length; i++) {
+            int lostStudent = lost[i];
+            
+            if (reserveSet.contains(lostStudent)) {
+                reserveSet.remove(lostStudent);
+                additionalCount++;
+            } else if (reserveSet.contains(lostStudent - 1) && !lostSet.contains(lostStudent - 1)) {
+                reserveSet.remove(lostStudent - 1);
+                additionalCount++;
+            } else if (reserveSet.contains(lostStudent + 1) && !lostSet.contains(lostStudent + 1)) {
+                reserveSet.remove(lostStudent + 1);
+                additionalCount++;
+            }
+        }
+        
+        return n - lost.length + additionalCount;
+    }
+    
+    private Set<Integer> toSet(int[] array) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < array.length; i++) {
+            set.add(array[i]);
+        }
+        return set;
+    }
 }
