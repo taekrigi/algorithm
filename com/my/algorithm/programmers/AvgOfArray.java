c587b3fa272ae751483690c9f3fe7b85c1b737b4
taekrigi <taekrigi@gmail.com>
2021-04-07 21:58:16 +0900

    avg

--- com/my/algorithm/programmers/AvgOfArray.java
+++ com/my/algorithm/programmers/AvgOfArray.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 public class AvgOfArray {
 	
 	public double solution(int[] arr) {
-        double answer = 0;
         return Arrays.stream(arr).average().orElse(0);
     }
 
