c3a74c606cccf51e9c7ac1747c5bf382eee0ed65
taekrigi <taekrigi@gmail.com>
2021-04-07 21:58:37 +0900

    Collatz

--- com/my/algorithm/programmers/CollatzConjecture.java
+++ com/my/algorithm/programmers/CollatzConjecture.java
@@ -1,27 +1,31 @@
 package my.algorithm.programmers;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class CollatzConjecture {
 	public int solution(long num) {
         int repeat = 0;
         boolean isOne = false;
         
         for (int i = 0; i < 500; i++) {
-            repeat++;
-            if (isEven(num)) {
-                num = num / 2;
-            } else {
-                num = num * 3 + 1;
-            }
-
             if (num == 1) {
                 isOne = true;
                 break;
             }
+            num = isEven(num) ? num / 2 : num * 3 + 1;
+            repeat++;
         }
         return !isOne ? -1 : repeat;
     }
     
     private boolean isEven(long num) {
         return num % 2 == 0;
+    }
+    
+    @Test
+    public void test() {
+    	assertEquals(solution(1), 0);
     }
 }
