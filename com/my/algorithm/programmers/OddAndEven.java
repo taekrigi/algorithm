8dd5fb7def73e525ee89bc97b2d391bba474e6d1
taekrigi <taekrigi@gmail.com>
2021-04-07 21:39:25 +0900

    odd and even

---
+++ com/my/algorithm/programmers/OddAndEven.java
@@ -0,0 +1,10 @@
+package my.algorithm.programmers;
+
+// https://programmers.co.kr/learn/courses/30/lessons/12937
+public class OddAndEven {
+	
+	public String solution(int num) {
+        return num % 2 == 0 ? "Even" : "Odd";
+    }
+
+}
