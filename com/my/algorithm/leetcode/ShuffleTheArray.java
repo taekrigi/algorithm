d87a654ae7d21f7e5166718be8709466b8d3c5f7
taekrigi <taekrigi@gmail.com>
2021-04-15 00:38:08 +0900

    100.00% faster...!

---
+++ com/my/algorithm/leetcode/ShuffleTheArray.java
@@ -0,0 +1,12 @@
+package my.algorithm.leetcode;
+
+public class ShuffleTheArray {
+	public int[] shuffle(int[] nums, int n) {
+        int[] answer = new int[nums.length];
+        for (int i = 0, j = 0; i < nums.length; i += 2, j++) {
+        	answer[i] = nums[j];
+        	answer[i + 1] = nums[n + j];
+        }
+        return answer;
+    }
+}
