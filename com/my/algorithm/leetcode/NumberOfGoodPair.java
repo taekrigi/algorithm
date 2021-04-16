baaca5cc63f7e3644155f329310de3a5af44d9e1
taekrigi <taekrigi@gmail.com>
2021-04-16 23:30:30 +0900

    소스정렬

--- com/my/algorithm/leetcode/NumberOfGoodPair.java
+++ com/my/algorithm/leetcode/NumberOfGoodPair.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 // https://leetcode.com/problems/number-of-good-pairs/
 public class NumberOfGoodPair {
+	
 	public int numIdenticalPairs(int[] nums) {
 		Map<Integer, Integer> pairs = new HashMap<>();
 		for (int num : nums) {
@@ -13,14 +14,14 @@ public class NumberOfGoodPair {
 		
 		int sum = 0;
 		for (Map.Entry<Integer, Integer> entry : pairs.entrySet()) {
-            if (!isOnlyOne(entry.getValue())) {
-               sum += sumSequence(entry.getValue()); 
-            }
+			if (!isOnlyOne(entry.getValue())) {
+				sum += sumSequence(entry.getValue());
+			}
 		}
 		
 		return sum;
-    }
-	
+	}
+
 	private int sumSequence(int num) {
 		return num * (num - 1) / 2;
 	}
